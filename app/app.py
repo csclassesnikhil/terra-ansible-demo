e0b9c9711bc16c7e8afd58c573faad2a8ae6ed31
csclassesnikhil <csclassesnikhil@gmail.com>
2025-12-13 16:46:57 -0800

    master commit

---
+++ app/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+return "Hello from Terraform + Ansible + GitHub Actions!"
+
+
+app.run(host="0.0.0.0", port=5000)
