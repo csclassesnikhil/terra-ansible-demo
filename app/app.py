f01abb367a73aaf3bb0773930fbc69673493ef1b
csclassesnikhil <csclassesnikhil@gmail.com>
2025-12-13 17:42:20 -0800

    bebo commit

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-return "Hello from Terraform + Ansible + GitHub Actions!"
+   return "Hello from Terraform + Ansible + GitHub Actions!"
 
 
 app.run(host="0.0.0.0", port=5000)
